257eba1417a9b1f7ba9fb9b3ed32633c444de603
Devbot0716 <pzepher@gmail.com>
2017-12-14 15:38:27 -0700

    Home page content is centered and doing everything I want to so far

--- public/js/services/mainSvc.js
+++ public/js/services/mainSvc.js
@@ -2,5 +2,9 @@ angular.module('app')
   .service('mainSvc', function($http){
 
     const localhost = 'http://localhost:3000';
-
+    function test (){
+      $http.get('/test')
+      .then( (res) => console.log(res) );
+    }
+    test();
   });
--- server/server.js
+++ server/server.js
@@ -8,19 +8,16 @@ const express = require('express')
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-massive(process.env.CONNECTION_STRING).then(db => app.set('db', db)).catch((err)=>console.log(err));
+// massive(process.env.CONNECTION_STRING).then(db => app.set('db', db)).catch((err)=>console.log(err));
 
-app.use((req, res, next)=> {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, x-access-token , X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.use(express.static('./public'));
 
+app.get('/test', (req, res, next)=>{
+  console.log('hello');
+  res.sendStatus(200);
+})
 
-
-const port = process.env.PORT || 3000
-app.listen(port, ( )=>{
+const port = /*process.env.PORT ||*/ 3000;
+app.listen(port, ( ) => {
   console.log("listening on port " + port);
 });
