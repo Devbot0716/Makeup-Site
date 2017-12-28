1532078759303b43f3cfd8249502fd5b65345dcb
Devbot0716 <pzepher@gmail.com>
2017-12-28 00:24:40 -0700

    pricing page progress

--- public/js/services/mainSvc.js
+++ public/js/services/mainSvc.js
@@ -1,10 +1,10 @@
 angular.module('app')
   .service('mainSvc', function($http){
 
-    const localhost = 'http://localhost:3000';
-    function test (){
-      $http.get('/test')
-      .then( (res) => console.log(res) );
-    }
-    test();
+    // const localhost = 'http://localhost:3000';
+    // function test (){
+    //   $http.get('/test')
+    //   .then( (res) => console.log(res) );
+    // }
+    // test();
   });
