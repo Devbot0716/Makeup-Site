07333a9c7f0f78353157ed4cde5ca188453128cd
Devbot0716 <pzepher@gmail.com>
2018-01-08 13:53:37 -0700

    added config.js to test DB

---
+++ server/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  database:'postgres://mohmwjdoviiuxs:659e0a2e46dcc32a32fca4fe299c5fee395f92418ddc25837cc95c26bc818ce6@ec2-23-21-164-107.compute-1.amazonaws.com:5432/d2ou2j8hefbnqf?ssl=true'
+}
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
     , bodyParser = require('body-parser')
     , massive = require('massive')
-    // , config = require('./config.js')
+    , config = require('./config.js')
     , controller = require('./serverCtrl.js')
     , cors = require('cors');
              require('dotenv').config();
