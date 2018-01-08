725c9cebaf190ba878af1051b5bc7534a7292ea4
Devbot0716 <pzepher@gmail.com>
2018-01-08 16:09:57 -0700

    images being served

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
+require('dotenv').config();
+
 const express = require('express')
     , bodyParser = require('body-parser')
     , massive = require('massive')
-    , config = require('./config.js')
     , controller = require('./serverCtrl.js')
     , cors = require('cors');
-             require('dotenv').config();
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-massive(process.env.CONNECTION_STRING).then(db => app.set('db', db)).catch((err)=>console.log(err));
+massive(process.env.CONNECTION_STRING).then(db =>{
+   app.set('db', db)}).catch((err)=>console.log(err));
 
-app.use(express.static('./public'));
+app.use(express.static('./public')); //serves front end
 
 // app.get('/test', (req, res, next)=>{
 //   console.log('hello');
@@ -19,7 +21,7 @@ app.use(express.static('./public'));
 
 //ENDPOINTS
 app.get('/getAll', controller.getAll);
-app.get('/getDetails/:ID', controller.getDetails);
+// app.get('/getDetails/:ID', controller.getDetails);
 app.get('/getImages/:Type', controller.getImages);
 
 const port = process.env.PORT || 3000;
